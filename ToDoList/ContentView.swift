777c4734243a658df953909b88ede1988a3edfba
Alicia Chiang <aliciawchiang@gmail.com>
2024-05-28 06:30:30 -0700

    hello

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -6,16 +6,52 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @State private var showNewTask = false
+    @Query var toDos: [ToDoItem]
+    @Environment(\.modelContext) var modelContext
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Text("To Do List")
+                    .font(.system(size: 40))
+                    .fontWeight(.black)
+                Spacer()
+                Button {
+                    self.showNewTask = true
+                    withAnimation {
+                        self.showNewTask = true
+                    }
+                } label: {
+                    Text("+")
+                }
+            }
+            .padding()
+            Spacer()
         }
-        .padding()
+        if showNewTask {
+            NewToDoView(toDoItem: ToDoItem(title: "", isImportant: false), showNewTask: $showNewTask)
+        }
+        
+        List {
+            ForEach (toDos) { toDoItem in
+                if toDoItem.isImportant == true {
+                    Text("‼️" + toDoItem.title)
+                } else {
+                    Text(toDoItem.title)
+                }
+            }
+            .onDelete(perform: deleteToDo)
+        }
+        .listStyle(.plain)
+    }
+    func deleteToDo(at offsets: IndexSet) {
+        for offset in offsets {
+            let toDoItem = toDos[offset]
+            modelContext.delete(toDoItem)
+        }
     }
 }
 
--- ToDoList/NewToDoView.swift
+++ ToDoList/NewToDoView.swift
@@ -6,13 +6,44 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct NewToDoView: View {
+    @Bindable var toDoItem: ToDoItem
+    @Environment(\.modelContext) var modelContext
+    @Binding var showNewTask: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Task title:")
+                .bold()
+                .padding()
+            TextField("Enter the task description...", text: $toDoItem.title, axis: .vertical)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Toggle(isOn: $toDoItem.isImportant) {
+                Text("Is it imporant?")
+                    .padding()
+                Button {
+                    addToDo()
+                    self.showNewTask = false
+                } label: {
+                    Text("Save")
+                }
+            }
+        }
+        .padding()
+        Spacer()
+    }
+    func addToDo(){
+        let toDo = ToDoItem(title: toDoItem.title, isImportant: toDoItem.isImportant)
+        modelContext.insert(toDo)
     }
 }
 
 #Preview {
-    NewToDoView()
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try! ModelContainer(for: ToDoItem.self, configurations: config)
+
+    let toDo = ToDoItem(title: "Example ToDo", isImportant: false)
+    return NewToDoView(toDoItem: toDo, showNewTask: .constant(true))
+        .modelContainer(container)
 }
